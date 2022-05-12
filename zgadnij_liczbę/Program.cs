a7042f196d595ea5ef1c39e3733c49ab5278b3b5
3115566 <3115566@TE-GDA-D-D37H07>
2022-05-12 12:38:26 +0200

    Dodaj pliki projektów.

---
+++ zgadnij_liczbę/Program.cs
@@ -0,0 +1,31 @@
+﻿Random mojaliczba = new Random();
+int min = 1;
+int max = 10;
+int losowa;
+int liczba;
+bool start = true;
+
+while (start)
+{
+
+    losowa = 0;
+    liczba = mojaliczba.Next(min, max);
+
+    while (losowa != liczba)
+    {
+        Console.WriteLine("podaj liczbę pomiędzy 1- 10: ");
+        losowa = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Podaj:");
+        if (losowa > liczba)
+        {
+            Console.WriteLine(losowa + "jest większa , sprobuj ponownie");
+        }
+        else if (losowa < liczba)
+        {
+            Console.WriteLine(losowa + " jest mniejsza , sprobuj ponownie");
+        }
+
+    }
+    Console.WriteLine("liczba:" + liczba);
+    Console.WriteLine("Wygrałeś gratulacje!");
+}
